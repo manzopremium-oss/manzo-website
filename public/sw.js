2ab454f3be88b3ec6ae46776e64b29e891692acd
wazeer <wazeert13@gmail.com>
2026-02-21 16:24:08 +0530

    Fix PWA installation issue by enhancing service worker and manifest metadata

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,38 @@
+const CACHE_NAME = 'manzo-v1';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/manifest.json',
+    '/logo-white.webp'
+];
+
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    event.waitUntil(self.clients.claim());
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-    // Basic pass-through for now
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
